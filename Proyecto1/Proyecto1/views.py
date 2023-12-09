53554882d7477bc6aa5fdd3dbcc37775258e6571
Nicolas Machado <nicolas5905code@gmail.com>
2023-12-09 00:46:45 -0300

    version 1.03 (error)

--- Proyecto1/Proyecto1/views.py
+++ Proyecto1/Proyecto1/views.py
@@ -12,3 +12,20 @@ def dia_de_hoy(request):
 def miNombreEs(self, nombre):
     documentoDeTexto = f"Mi nombre es: <br><br> {nombre}"
     return HttpResponse(documentoDeTexto)
+
+def probando_template(request):
+    nombre = "Nicolas"
+    apellido = "Machado"
+    lista_de_notas = [1,2,3,4,8,3,2]
+
+    diccionario = {"nombre": nombre, "apellido": apellido, "hoy": datetime.datetime.now(), "notas": lista_de_notas}
+
+    mi_html = open("\Users\nicol\TuPrimeraPaginaMachado\Proyecto1\Proyecto1\templates\template1.html", "r")
+
+    mi_html.close()
+
+    mi_contexto = Context(diccionario)
+
+    documento = plantilla.render(mi_contexto)
+
+    return HttpResponse(documento)
