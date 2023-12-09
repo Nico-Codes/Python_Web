c14d212d5d59d7c6925491af1fc72865dd628ede
Nicolas Machado <nicolas5905code@gmail.com>
2023-12-08 23:53:49 -0300

    Version 1.02

--- Proyecto1/Proyecto1/urls.py
+++ Proyecto1/Proyecto1/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from Proyecto1.views import saludo
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('saludo/', saludo),
 ]
